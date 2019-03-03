9ccba8900c9a7da24aafad7d37779cc8f7b1b59e
DavidM17 <davicaza17@gmail.com>
2019-03-03 11:10:58 -0500

    first commit

---
+++ build/keys.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        host: 'testdb.c58xpfskqzck.us-east-1.rds.amazonaws.com',
+        user: 'diseno1',
+        database: 'dbb',
+        password: 'diseno12345',
+        port: 3306
+    }
+};
