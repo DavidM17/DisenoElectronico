e3ca14673a465d82eb41f2ed4f36acb76a22caec
DavidM17 <48101034+DavidM17@users.noreply.github.com>
2019-05-24 17:39:53 -0500

    Update keys.js

--- build/keys.js
+++ build/keys.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'mydbinstance.c58xpfskqzck.us-east-1.rds.amazonaws.com',
-        user: 'diseno',
+        host: '',
+        user: '',
         database: 'coord_db',
-        password: 'diseno10',
+        password: '',
         port: 3306
     }
 };
